39d7c5e7b6d692929190b926af64c2fc3898bc5c
Bryan Yehuda <bryanyehuda@gmail.com>
2021-10-25 22:57:56 +0700

    Adding Executable

---
+++ executeable/modul.sh
@@ -0,0 +1,590 @@
+#!/bin/bash
+Foosha(){
+    apt-get update
+    iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE -s 10.45.0.0/16
+
+}
+
+EniesLobby() {
+echo "nameserver 192.168.122.1" > /etc/resolv.conf
+apt-get update
+apt-get install bind9 -y
+echo 'zone "franky.t07.com" {
+type master;
+file "/etc/bind/kaizoku/franky.t07.com";
+};'
+# nomor 2
+echo 'zone "franky.t07.com" {
+        type master;
+        file "/etc/bind/kaizoku/franky.t07.com";
+};' > /etc/bind/named.conf.local
+mkdir /etc/bind/kaizoku
+echo "
+\$TTL    604800
+@       IN      SOA     franky.t07.com. root.franky.t07.com. (
+                        2021100401      ; Serial
+                        604800          ; Refresh
+                        86400           ; Retry
+                        2419200         ; Expire
+                        604800 )        ; Negative Cache TTL
+;
+@               IN      NS      franky.t07.com.
+@               IN      A       10.45.2.2 ; IP EniesLobby
+www             IN      CNAME   franky.t07.com.
+" > /etc/bind/kaizoku/franky.t07.com
+service bind9 restart
+
+# nomor 3
+echo "
+\$TTL    604800
+@       IN      SOA     franky.t07.com. root.franky.t07.com. (
+                        2021100401      ; Serial
+                        604800          ; Refresh
+                        86400           ; Retry
+                        2419200         ; Expire
+                        604800 )        ; Negative Cache TTL
+;
+@               IN      NS      franky.t07.com.
+@               IN      A       10.45.2.2 ; IP EniesLobby
+www             IN      CNAME   franky.t07.com.
+super           IN      A       10.45.2.4 ; IP skype
+www.super       IN      CNAME   super.franky.t07.com.
+" > /etc/bind/kaizoku/franky.t07.com
+service bind9 restart
+
+# nomor 4
+
+echo '
+zone "franky.t07.com" {
+        type master;
+        file "/etc/bind/kaizoku/franky.t07.com";
+};
+
+zone "2.45.10.in-addr.arpa" {
+        type master;
+        file "/etc/bind/kaizoku/2.45.10.in-addr.arpa";
+};' > /etc/bind/named.conf.local
+
+echo "
+\$TTL    604800
+@       IN      SOA     franky.t07.com. root.franky.t07.com. (
+                        2021100401      ; Serial
+                        604800          ; Refresh
+                        86400         ; Retry
+                        2419200         ; Expire
+                        604800 )       ; Negative Cache TTL
+;
+2.45.10.in-addr.arpa.   IN      NS      franky.t07.com.
+2                       IN      PTR     franky.t07.com.
+"> /etc/bind/kaizoku/2.45.10.in-addr.arpa
+service bind9 restart
+
+
+# nomor 5
+
+echo '
+zone "franky.t07.com" {
+        type master;
+        notify yes;
+        also-notify {10.45.2.3;};  //Masukan IP Water7 tanpa tanda petik
+        allow-transfer {10.45.2.3;}; // Masukan IP Water7 tanpa tanda petik
+        file "/etc/bind/kaizoku/franky.t07.com";
+};
+
+zone "2.45.10.in-addr.arpa" {
+        type master;
+        file "/etc/bind/kaizoku/2.45.10.in-addr.arpa";
+};' > /etc/bind/named.conf.local
+service bind9 restart
+
+# nomor 6
+echo "
+\$TTL    604800
+@       IN      SOA     franky.t07.com. root.franky.t07.com. (
+                        2021100401      ; Serial
+                        604800          ; Refresh
+                        86400           ; Retry
+                        2419200         ; Expire
+                        604800 )        ; Negative Cache TTL
+;
+@               IN      NS      franky.t07.com.
+@               IN      A       10.45.2.4 ; IP skypea
+www             IN      CNAME   franky.t07.com.
+super           IN      A       10.45.2.4 ; IP skypea
+www.super       IN      CNAME   super.franky.t07.com.
+ns1             IN      A       10.45.2.3; IP Water7
+mecha           IN      NS      ns1
+"> /etc/bind/kaizoku/franky.t07.com
+
+echo "
+options {
+        directory \"/var/cache/bind\";
+
+        allow-query{any;};
+        auth-nxdomain no;    # conform to RFC1035
+        listen-on-v6 { any; };
+};
+" > /etc/bind/named.conf.options
+
+echo '
+zone "franky.t07.com" {
+        type master;
+        //notify yes;
+        //also-notify {10.45.2.3;};  Masukan IP Water7 tanpa tanda petik
+        file "/etc/bind/kaizoku/franky.t07.com";
+        allow-transfer {10.45.2.3;}; // Masukan IP Water7 tanpa tanda petik
+};
+
+zone "2.45.10.in-addr.arpa" {
+        type master;
+        file "/etc/bind/kaizoku/2.45.10.in-addr.arpa";
+};
+' >  /etc/bind/named.conf.local
+
+service bind9 restart
+}
+
+Water7(){
+echo "nameserver 192.168.122.1" > /etc/resolv.conf
+apt-get update
+
+# nomor 5
+apt-get update
+apt-get install bind9 -y
+echo '
+zone "franky.t07.com" {
+        type slave;
+        masters { 10.45.2.2; }; // Masukan IP EniesLobby tanpa tanda petik
+        file "/var/lib/bind/franky.t07.com";
+};
+' > /etc/bind/named.conf.local
+service bind9 restart
+
+# nomor 6
+
+echo "
+options {
+        directory \"/var/cache/bind\";
+        allow-query{any;};
+        auth-nxdomain no;    # conform to RFC1035
+        listen-on-v6 { any; };
+};
+" > /etc/bind/named.conf.options
+echo '
+zone "franky.t07.com" {
+        type slave;
+        masters { 10.45.2.2; }; // Masukan IP EniesLobby tanpa tanda petik
+        file "/var/lib/bind/franky.t07.com";
+};
+
+zone "mecha.franky.t07.com"{
+        type master;
+        file "/etc/bind/sunnygo/mecha.franky.t07.com";
+};
+'> /etc/bind/named.conf.local
+mkdir /etc/bind/sunnygo
+echo "
+\$TTL    604800
+@       IN      SOA     mecha.franky.t07.com. root.mecha.franky.t07.com. (
+                        2021100401      ; Serial
+                        604800         ; Refresh
+                        86400         ; Retry
+                        2419200         ; Expire
+                        604800 )       ; Negative Cache TTL
+;
+@               IN      NS      mecha.franky.t07.com.
+@               IN      A       10.45.2.4       ;ip skypie
+www             IN      CNAME   mecha.franky.t07.com.
+" > /etc/bind/sunnygo/mecha.franky.t07.com
+service bind9 restart
+
+
+# Nomor 7
+echo "
+\$TTL    604800
+@       IN      SOA     mecha.franky.t07.com. root.mecha.franky.t07.com. (
+                        2021100401      ; Serial
+                        604800         ; Refresh
+                        86400         ; Retry
+                        2419200         ; Expire
+                        604800 )       ; Negative Cache TTL
+;
+@               IN      NS      mecha.franky.t07.com.
+@               IN      A       10.45.2.4       ;ip skypie
+www             IN      CNAME   mecha.franky.t07.com.
+general         IN      A       10.45.2.4       ;IP skypie
+www.general     IN      CNAME   general.mecha.franky.t07.com.
+" > /etc/bind/sunnygo/mecha.franky.t07.com
+service bind9 restart
+
+}
+
+Loguetown(){
+echo "nameserver 192.168.122.1" > /etc/resolv.conf
+apt-get update
+apt-get install dnsutils -y
+apt-get install lynx -y
+# nomor 5
+echo "
+nameserver 10.45.2.2
+nameserver 10.45.2.3
+nameserver 10.45.2.4
+
+" > /etc/resolv.conf
+}
+
+Alabasta(){
+echo "nameserver 192.168.122.1" > /etc/resolv.conf
+apt-get update
+apt-get install dnsutils -y
+apt-get install lynx -y
+# nomor 5
+echo "
+nameserver 10.45.2.2
+nameserver 10.45.2.3
+nameserver 10.45.2.4
+
+" > /etc/resolv.conf
+}
+
+Skypie(){
+echo "nameserver 192.168.122.1" > /etc/resolv.conf
+apt-get update
+# nomor 8
+apt-get install apache2 -y
+service apache2 start
+apt-get install php -y
+apt-get install libapache2-mod-php7.0 -y
+service apache2 
+apt-get install ca-certificates openssl -y
+echo "
+<VirtualHost *:80>
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/franky.t07.com
+        ServerName franky.t07.com
+        ServerAlias www.franky.t07.com
+
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+" > /etc/apache2/sites-available/franky.t07.com.conf
+a2ensite franky.t07.com
+mkdir /var/www/franky.t07.com
+cp -r /root/Praktikum-Modul-2-Jarkom/franky/. /var/www/franky.t07.com
+service apache2 restart
+
+# nomor 9
+a2enmod rewrite
+service apache2 restart
+echo "
+RewriteEngine On
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule (.*) /index.php/\$1 [L]
+" >/var/www/franky.t07.com/.htaccess
+echo "
+<VirtualHost *:80>
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/franky.t07.com
+        ServerName franky.t07.com
+        ServerAlias www.franky.t07.com
+
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+
+        <Directory /var/www/franky.t07.com>
+                Options +FollowSymLinks -Multiviews
+                AllowOverride All
+        </Directory>
+</VirtualHost>
+" > /etc/apache2/sites-available/franky.t07.com.conf
+service apache2 restart
+
+# nomor 10
+echo "
+<VirtualHost *:80>
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/super.franky.t07.com
+        ServerName super.franky.t07.com
+        ServerAlias www.super.franky.t07.com
+
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+
+        <Directory /var/www/franky.t07.com>
+                Options +FollowSymLinks -Multiviews
+                AllowOverride All
+        </Directory>
+</VirtualHost>
+" > /etc/apache2/sites-available/super.franky.t07.com.conf
+a2ensite super.franky.t07.com
+mkdir /var/www/super.franky.t07.com
+cp -r /root/Praktikum-Modul-2-Jarkom/super.franky/. /var/www/super.franky.t07.com
+service apache2 restart
+echo "<?php echo 'yes nomor 10' ?>" > /var/www/super.franky.t07.com/index.php
+
+# nomor 11
+
+echo "
+<VirtualHost *:80>
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/super.franky.t07.com
+        ServerName super.franky.t07.com
+        ServerAlias www.super.franky.t07.com
+
+        <Directory /var/www/super.franky.t07.com/public>
+                Options +Indexes
+        </Directory>
+
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+
+        <Directory /var/www/franky.t07.com>
+                Options +FollowSymLinks -Multiviews
+                AllowOverride All
+        </Directory>
+</VirtualHost>
+" > /etc/apache2/sites-available/super.franky.t07.com.conf
+service apache2 restart
+
+# nomor 12
+echo "
+<VirtualHost *:80>
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/super.franky.t07.com
+        ServerName super.franky.t07.com
+        ServerAlias www.super.franky.t07.com
+
+        ErrorDocument 404 /error/404.html
+        ErrorDocument 500 /error/404.html
+        ErrorDocument 502 /error/404.html
+        ErrorDocument 503 /error/404.html
+        ErrorDocument 504 /error/404.html
+
+        <Directory /var/www/super.franky.t07.com/public>
+                Options +Indexes
+        </Directory>
+
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+
+        <Directory /var/www/franky.t07.com>
+                Options +FollowSymLinks -Multiviews
+                AllowOverride All
+        </Directory>
+</VirtualHost>
+" > /etc/apache2/sites-available/super.franky.t07.com.conf
+service apache2 restart
+
+# nomor 13
+echo "
+<VirtualHost *:80>
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/super.franky.t07.com
+        ServerName super.franky.t07.com
+        ServerAlias www.super.franky.t07.com
+
+        ErrorDocument 404 /error/404.html
+        ErrorDocument 500 /error/404.html
+        ErrorDocument 502 /error/404.html
+        ErrorDocument 503 /error/404.html
+        ErrorDocument 504 /error/404.html
+
+        <Directory /var/www/super.franky.t07.com/public>
+                Options +Indexes
+        </Directory>
+
+        Alias \"/js\" \"/var/www/super.franky.t07.com/public/js\"
+
+
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+
+        <Directory /var/www/franky.t07.com>
+                Options +FollowSymLinks -Multiviews
+                AllowOverride All
+        </Directory>
+</VirtualHost>
+" > /etc/apache2/sites-available/super.franky.t07.com.conf
+service apache2 restart
+
+# nomor 14
+
+echo "
+<VirtualHost *:15000>
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/general.mecha.franky.t07.com
+        ServerName general.mecha.franky.t07.com
+        ServerAlias www.general.mecha.franky.t07.com
+
+
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+<VirtualHost *:15500>        
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/general.mecha.franky.t07.com
+        ServerName general.mecha.franky.t07.com
+        ServerAlias www.general.mecha.franky.t07.com
+        
+
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+" > /etc/apache2/sites-available/general.mecha.franky.t07.com.conf
+a2ensite general.mecha.franky.t07.com
+service apache2 restart
+mkdir /var/www/general.mecha.franky.t07.com
+cp -r /root/Praktikum-Modul-2-Jarkom/general.mecha.franky/. /var/www/general.mecha.franky.t07.com/
+echo "
+<?php
+        echo 'selamat 14';
+?>
+" > /var/www/general.mecha.franky.t07.com/index.php
+echo "
+# If you just change the port or add more ports here, you will likely also
+# have to change the VirtualHost statement in
+# /etc/apache2/sites-enabled/000-default.conf
+
+Listen 80
+Listen 15000
+Listen 15500
+<IfModule ssl_module>
+        Listen 443
+</IfModule>
+
+<IfModule mod_gnutls.c>
+        Listen 443
+</IfModule>
+" > /etc/apache2/ports.conf
+
+service apache2 restart
+
+# nomor 15
+htpasswd -c -b /etc/apache2/.htpasswd luffy onepiece
+
+echo "
+<VirtualHost *:15000>
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/general.mecha.franky.t07.com
+        ServerName general.mecha.franky.t07.com
+        ServerAlias www.general.mecha.franky.t07.com
+
+        <Directory \"/var/www/general.mecha.franky.t07.com\">
+                AuthType Basic
+                AuthName \"Restricted Content\"
+                AuthUserFile /etc/apache2/.htpasswd
+                Require valid-user
+        </Directory>
+
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+<VirtualHost *:15500>        
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/general.mecha.franky.t07.com
+        ServerName general.mecha.franky.t07.com
+        ServerAlias www.general.mecha.franky.t07.com
+        
+        <Directory \"/var/www/general.mecha.franky.t07.com\">
+                AuthType Basic
+                AuthName \"Restricted Content\"
+                AuthUserFile /etc/apache2/.htpasswd
+                Require valid-user
+        </Directory>
+        
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+" > /etc/apache2/sites-available/general.mecha.franky.t07.com.conf
+service apache2 restart
+
+# nomor 16
+
+echo "
+<VirtualHost *:80>
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/html
+
+        RewriteEngine On
+        RewriteCond %{HTTP_HOST} !^franky.t07.com$
+        RewriteRule /.* http://franky.t07.com/ [R]
+
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+
+</VirtualHost>
+" > /etc/apache2/sites-available/000-default.conf
+service apache2 restart
+
+# nomor 17
+echo "
+RewriteEngine On
+RewriteCond %{REQUEST_URI} ^/public/images/(.*)franky(.*)
+RewriteCond %{REQUEST_URI} !/public/images/franky.png
+RewriteRule /.* http://super.franky.t07.com/public/images/franky.png [L]
+" > /var/www/super.franky.t07.com/.htaccess
+
+echo "
+<VirtualHost *:80>
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/super.franky.t07.com
+        ServerName super.franky.t07.com
+        ServerAlias www.super.franky.t07.com
+
+        ErrorDocument 404 /error/404.html
+        ErrorDocument 500 /error/404.html
+        ErrorDocument 502 /error/404.html
+        ErrorDocument 503 /error/404.html
+        ErrorDocument 504 /error/404.html
+
+        <Directory /var/www/super.franky.t07.com/public>
+                Options +Indexes
+        </Directory>
+
+        Alias \"/js\" \"/var/www/super.franky.t07.com/public/js\"
+
+        <Directory /var/www/super.franky.t07.com>
+                Options +FollowSymLinks -Multiviews
+                AllowOverride All
+        </Directory>
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+
+        <Directory /var/www/franky.t07.com>
+                Options +FollowSymLinks -Multiviews
+                AllowOverride All
+        </Directory>
+</VirtualHost>
+" > /etc/apache2/sites-available/super.franky.t07.com.conf
+service apache2 restart
+
+}
+
+if [ $HOSTNAME == "Foosha" ]
+then
+    Foosha
+elif [ $HOSTNAME == "EniesLobby" ]
+then
+    EniesLobby
+elif [ $HOSTNAME == "Water7" ]
+then
+    Water7
+elif [ $HOSTNAME == "Loguetown" ]
+then
+    Loguetown
+elif [ $HOSTNAME == "Alabasta" ]
+then
+    Alabasta
+elif [ $HOSTNAME == "Skypie" ]
+then
+    Skypie
+fi
